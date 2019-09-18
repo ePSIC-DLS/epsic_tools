a7693eb3d6b9f41e3d59294ed3159daab6564f0d
Tom Slater <TomSlater@users.noreply.github.com>
2019-09-18 16:16:11 +0100

    Update load_ptyr.py
    Changed colormap to viridis and removed ticks and tick labels as Mohsen requested.

--- ProcessingTools/load_ptyr.py
+++ ProcessingTools/load_ptyr.py
@@ -20,7 +20,9 @@ def plot_ptyr(filename):
     probe_data = probe[dataname]
     probe_data_arr = probe_data['data']
     
-    plt.subplot(141); plt.imshow(data_arr[0].real); plt.title('Object Amplitude');
-    plt.subplot(142); plt.imshow(data_arr[0].imag); plt.title('Object Phase'); 
-    plt.subplot(143); plt.imshow(probe_data_arr[0].real); plt.title('Probe Amplitude');
-    plt.subplot(144); plt.imshow(probe_data_arr[0].imag); plt.title('Probe Phase');
+    plt.rcParams['image.cmap'] = 'viridis'
+    
+    plt.subplot(141); plt.imshow(data_arr[0].real); plt.title('Object Amplitude'); plt.axis('off');
+    plt.subplot(142); plt.imshow(data_arr[0].imag); plt.title('Object Phase'); plt.axis('off');
+    plt.subplot(143); plt.imshow(probe_data_arr[0].real); plt.title('Probe Amplitude'); plt.axis('off');
+    plt.subplot(144); plt.imshow(probe_data_arr[0].imag); plt.title('Probe Phase'); plt.axis('off');
