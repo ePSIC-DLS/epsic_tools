6473a5ea5b29c5efac2e3873bd2204f69c2cdc0e
chrisallen80 <42769062+chrisallen80@users.noreply.github.com>
2020-02-14 10:25:48 +0000

    Tidy up comments - Gaussian blur bug fix

--- epsic_tools/toolbox/ptychography/load_pycho_series.py
+++ epsic_tools/toolbox/ptychography/load_pycho_series.py
@@ -59,64 +59,18 @@ def load_series(pn,crop_to, sort_by = 'rot', blur = 0, verbose = False):
     d_s, p_s, d_s_fft, rad_fft, r_s, s_s, e_s = load_series(pn,sort_by = 'rot', crop_to = 80)
     hs.plot.plot_signals([d_s,p_s,d_s_fft, rad_fft], navigator_list=[r_s,s_s, e_s,None])
 
-    ##set positions
-    plt.figure(1)
-    plt.title('probe array rotation angle')
-    plt.xlabel('data set number')
-    plt.ylabel('rotation angle (deg)')
-    plt.tight_layout()
-    plt.grid()
-    mngr = plt.get_current_fig_manager()
-    geom = mngr.window.geometry()
-    x,y,dx,dy = geom.getRect()
-    
-    mngr.window.setGeometry(50,100,dx, dy)
-    
-    plt.figure(2)
-    mngr = plt.get_current_fig_manager()
-    mngr.window.setGeometry(700,100,dx, dy)
-    
-    plt.figure(3)
-    mngr = plt.get_current_fig_manager()
-    mngr.window.setGeometry(50,700,dx, dy)
-    plt.grid()
-    plt.title('step size')
-    plt.xlabel('data set number')
-    plt.ylabel('dx')
-    plt.tight_layout()
-    
-    plt.figure(4)
-    mngr = plt.get_current_fig_manager()
-    mngr.window.setGeometry(700,700,dx, dy)
-    
-    plt.figure(5)
-    mngr = plt.get_current_fig_manager()
-    mngr.window.setGeometry(50,800,dx, dy)
-    plt.title('error')
-    plt.xlabel('data set number')
-    plt.ylabel('error')
-    plt.tight_layout()
-    
-    plt.figure(6)
-    mngr = plt.get_current_fig_manager()
-    mngr.window.setGeometry(1350,100,dx, dy)
+
     '''
-    #pn = r'Y:\2020\cm26481-1\processing\Merlin\20200130_80kV_graphene_600C_pty\cluster\processing\pycho'
-    #r'Y:\2019\cm22979-6\processing\Merlin\Merlin\20191001_15kV_ptycho\cluster\processing\pycho'
-    #'Y:\2019\cm22979-6\processing\Merlin\Merlin\20191001_15kV_ptycho\MoS2_700C\scan_step_refine\processing\pycho'
-    #Y:\2019\cm22979-6\processing\Merlin\Merlin\20191001_15kV_ptycho\MoS2_700C\rot_step_refine\processing\pycho'
-    #sort_by = 'rot' # 'rot' or 'step' - parameter by which to sort the data
     pn = pn +'\*.hdf'
-    #pn = pn + '\*.json'
     #build list of files
     fp_list = glob.glob(pn)
     len_dat = len(fp_list)
-    n = 0 
-    #crop_to = 80
+    n = 0 # counter
     
-    
+    # iterate through files
     for this_fp in fp_list: 
         fj = this_fp[:-4] + '.json'
+        #open json file
         with open(fj) as r:
             params = json.load(r)
         with h5py.File(this_fp, 'r') as d5:
@@ -147,35 +101,38 @@ def load_series(pn,crop_to, sort_by = 'rot', blur = 0, verbose = False):
             #get error plot
             error = np.array(d5['entry_1']['process_1']['output_1']['error'])
             error = error[error != 0]
-            d5.close()
+            d5.close()  # probably not necessary but just in case!
         if n == 0:
             #initiate arrays if first itteration - make bigger than fist data to account for change in size
             shape_dat = int(10 * (np.ceil(dat.shape[0]/10) +1))
             shape_probe = int(10 * (np.ceil(probe.shape[0]/ 10)+1))
-            #print(n, shape_dat, shape_probe)
-            #print(n,len_dat, shape_dat, shape_dat)
+            
+            if verbose == True: 
+                print(n,len_dat, shape_dat, shape_dat)
             dat_arr = np.empty(shape = (2,len_dat, shape_dat, shape_dat))
             probe_arr = np.empty(shape = (2, len_dat, shape_probe, shape_probe))
             rot_arr = np.empty(shape = len_dat)
             step_arr =np.empty(shape = len_dat)
             err_arr = np.empty(shape = len_dat)
-        #print(n,len_dat, shape_dat)
+            
+        # calculate parameters to pad loaded data to same size as initiated array if necessary
         dat_diff =shape_dat - dat.shape[0] 
         pad_dat = int(np.ceil(dat_diff / 2))
         del_dat = int(pad_dat - np.floor(dat_diff / 2))
         
+        probe_diff =shape_probe - probe.shape[0] 
+        pad_probe = int(np.ceil(probe_diff / 2))
+        del_probe = int(pad_probe - np.floor(probe_diff / 2))
+        
+        # populate rotation, step and error array
         rot_arr[n] = float(params['process']['common']['scan']['rotation'])
         step_arr[n] = float(params['process']['common']['scan']['dR'][0])
         
         err_arr[n] = error[-1]
         
-        #print(n,len_dat, dat.shape[0] , shape_dat, del_dat, pad_dat, 'dx : ', )
         if verbose == True:
             print(n, ' step : ', step_arr[n], ', rot : ', rot_arr[n], ', err : ', err_arr[n])
-        probe_diff =shape_probe - probe.shape[0] 
-        pad_probe = int(np.ceil(probe_diff / 2))
-        del_probe = int(pad_probe - np.floor(probe_diff / 2))
-        #print(n, shape_dat, shape_probe, dat.shape, probe.shape, pad_dat)
+        # load data into arrays (padded if necessary)
         if pad_dat > 0:
             dat_arr[0,n,:,:] = np.pad(dat[del_dat:, del_dat:], pad_dat, 'edge') #object phase
             dat_arr[1,n,:,:] = np.pad(dat_m[del_dat:, del_dat:], pad_dat, 'edge')  #object mod   
@@ -200,54 +157,66 @@ def load_series(pn,crop_to, sort_by = 'rot', blur = 0, verbose = False):
         
         n = n+1
     
-    #define structured array and sort 
+    # define structured array and sort 
     w_type = np.dtype([('rot', 'float'), ('step', 'float')])
     w = np.empty(len(rot_arr), dtype = w_type)
     w['rot'] = rot_arr
     w['step'] = step_arr
-    #if sort_by == 'rot':
-    sort_ind = np.argsort(w, order = ('rot', 'step'))
-    #elif sort_by == 'step':
-     #   sort_ind = np.argsort(step_arr)
+    
+    if sort_by == 'rot':
+        sort_ind = np.argsort(w, order = ('rot', 'step'))
+    elif sort_by == 'step':
+        sort_ind = np.argsort(w, order = ('step', 'rot'))
+    
+    # re-order arrays    
     rot_sort = rot_arr[sort_ind]
     step_sort = step_arr[sort_ind]
     err_sort = err_arr[sort_ind]
     dat_sort =  dat_arr[:,sort_ind,:,:]
     probe_sort = probe_arr[:,sort_ind,:,:]
     
+    # unsorted to hs signals
+    '''
     d = hs.signals.Signal2D(data = dat_arr)
     p = hs.signals.Signal2D(data = probe_arr)
     r = hs.signals.Signal1D(data = rot_arr)
     s = hs.signals.Signal1D(data = step_arr)
     e = hs.signals.Signal1D(data = err_arr)
+    '''
     
+    #sorted to hs signals
     d_s = hs.signals.Signal2D(data = dat_sort)
     p_s = hs.signals.Signal2D(data = probe_sort)
     r_s = hs.signals.Signal1D(data = rot_sort)
     s_s = hs.signals.Signal1D(data = step_sort)
     e_s = hs.signals.Signal1D(data = err_sort)
     #crop
+    '''
     d.crop(axis = (2), start =int((shape_dat / 2) - (crop_to/2)), end = int((shape_dat / 2) + (crop_to/2) ))
     d.crop(axis = (3), start =int((shape_dat / 2) - (crop_to/2)), end = int((shape_dat / 2) + (crop_to/2) ))
+    '''
+    
     d_s.crop(axis = (2), start =int((shape_dat / 2) - (crop_to/2)), end = int((shape_dat / 2) + (crop_to/2) ))
     d_s.crop(axis = (3), start =int((shape_dat / 2) - (crop_to/2)), end = int((shape_dat / 2) + (crop_to/2) ))
     
     #gaussian blur 
-    d_s.map(gaussian, sigma = 1)
+    d_s.map(gaussian, sigma = blur)
     
+    # fft 
+    '''
     dat_fft = np.fft.fft2(d.data)
     dat_fft = np.fft.fftshift(dat_fft)
+    
     d_fft = hs.signals.Signal2D(data = np.log10(np.abs(dat_fft)**2))
     d_fft.data = np.flip(d_fft.data, axis = 0)
+    '''
     
     dat_sort_fft = np.fft.fft2(d_s.data)
     dat_sort_fft = np.fft.fftshift(dat_sort_fft)
     d_s_fft = hs.signals.Signal2D(data = np.log10(np.abs(dat_sort_fft)**2))
     d_s_fft.data = np.flip(d_s_fft.data, axis = 0)
-    fft_mask = np.zeros_like(d_fft.data, dtype = 'bool')
+    fft_mask = np.zeros_like(d_s_fft.data, dtype = 'bool')
     fft_shape = fft_mask.shape
-    #fft_mask[int(fft_shape[0]/2), :] = 1
-    #fft_mask[: ,int(fft_shape[1]/2)] = 1
     
     d_s_fft.inav[:,0].data[:,int(fft_shape[-1]/2), :] =0
     d_s_fft.inav[:,0].data[:,:,int(fft_shape[-2]/2)] =0
