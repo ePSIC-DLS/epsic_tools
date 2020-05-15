ea7c9a9850bf2c19ad4f941e18f47d7eeb4983db
Mohsen Danaie <mohsen.danaie@diamond.ac.uk>
2020-05-15 23:26:11 +0100

    mod of ptypy recon plot func

--- epsic_tools/toolbox/ptycho_utils.py
+++ epsic_tools/toolbox/ptycho_utils.py
@@ -13,6 +13,9 @@ import matplotlib.pyplot as plt
 
 
 def plot_ptyr(filename):
+    """
+    Plots the real and imaginary parts of a ptypy recon file
+    """
     f = h5py.File(filename,'r')
     content = f['content']
     obj = content['obj']
@@ -26,7 +29,7 @@ def plot_ptyr(filename):
     
     plt.rcParams['image.cmap'] = 'viridis'
     
-    plt.subplot(141); plt.imshow(data_arr[0].real); plt.title('Object Amplitude'); plt.axis('off');
-    plt.subplot(142); plt.imshow(data_arr[0].imag); plt.title('Object Phase'); plt.axis('off');
-    plt.subplot(143); plt.imshow(probe_data_arr[0].real); plt.title('Probe Amplitude'); plt.axis('off');
-    plt.subplot(144); plt.imshow(probe_data_arr[0].imag); plt.title('Probe Phase'); plt.axis('off');
+    plt.subplot(141); plt.imshow(data_arr[0].real); plt.title('Object real'); plt.axis('off');
+    plt.subplot(142); plt.imshow(data_arr[0].imag); plt.title('Object imaginary'); plt.axis('off');
+    plt.subplot(143); plt.imshow(probe_data_arr[0].real); plt.title('Probe real'); plt.axis('off');
+    plt.subplot(144); plt.imshow(probe_data_arr[0].imag); plt.title('Probe imaginary'); plt.axis('off');
