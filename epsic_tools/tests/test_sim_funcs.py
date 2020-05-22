7b43c9944e361839cdfde62f71cdfc2fe7de9330
Mohsen Danaie <mohsen.danaie@diamond.ac.uk>
2020-05-22 23:22:57 +0100

    fixing test_sim_funcs typo

--- epsic_tools/tests/test_sim_funcs.py
+++ epsic_tools/tests/test_sim_funcs.py
@@ -15,7 +15,7 @@ def test_parse_params_file():
     test_data_path = os.path.join(root_path , 'tests', 'test_data', 'test_sim.h5')
     test_params_path = os.path.join(root_path , 'tests', 'test_data', 'test_sim_params.txt')
     d = epsic.sim_utils.parse_params_file(test_params_path, test_data_path)
-    assert t['accel_voltage(eV)'] == 80000
+    assert d['accel_voltage(eV)'] == 80000
     
 
 
