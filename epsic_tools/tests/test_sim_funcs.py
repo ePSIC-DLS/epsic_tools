aa2d6e73f56c11f509ef4f6d038ab3bca11051c9
Mohsen Danaie <mohsen.danaie@diamond.ac.uk>
2020-05-22 21:21:29 +0100

    still trying to fix test_sim_funcs

--- epsic_tools/tests/test_sim_funcs.py
+++ epsic_tools/tests/test_sim_funcs.py
@@ -1,19 +1,19 @@
 
 import os
 import numpy.testing as nptest
-import epsic_tools.api as epsic
+import api as epsic
 
 def test_sim_to_hs():
     root_path = os.path.dirname(epsic.__file__)
-    test_data_path = os.join(root_path , 'toolbox', 'tests', 'test_data', 'test_sim.h5')
+    test_data_path = os.join(root_path , 'tests', 'test_data', 'test_sim.h5')
     d = epsic.sim_utils.sim_to_hs(test_data_path)
     shape = d.data.shape
     nptest.assert_equal(shape, (4, 4, 256, 256))
 
 def test_parse_params_file():
     root_path = os.path.dirname(epsic.__file__)
-    test_data_path = os.join(root_path , 'toolbox', 'tests', 'test_data', 'test_sim.h5')
-    test_params_path = os.join(root_path , 'toolbox', 'tests', 'test_data', 'test_sim_params.txt')
+    test_data_path = os.join(root_path , 'tests', 'test_data', 'test_sim.h5')
+    test_params_path = os.join(root_path , 'tests', 'test_data', 'test_sim_params.txt')
     d = epsic.sim_utils.parse_params_file(test_params_path, test_data_path)
     assert t['accel_voltage(eV)'] == 80000
     
