96b3c5e45cacc85820fdca8e322ed6cf4fb65816
Danaie, Mohsen (DLSLtd,RAL,SCI) <mohsen.danaie@diamond.ac.uk>
2023-10-04 11:49:53 +0100

    adding notebook utils

--- epsic_tools/api.py
+++ epsic_tools/api.py
@@ -7,4 +7,5 @@ from epsic_tools.toolbox import warp_3d
 from epsic_tools.toolbox import make_merlin_mask
 from epsic_tools.toolbox import sim_utils
 from epsic_tools.toolbox import ptycho_utils
+from epsic_tools.toolbox import notebook_utils
 from epsic_tools.iris_S3 import ePSIC_S3
